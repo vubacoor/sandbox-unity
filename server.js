301f824037230daf89e90672807e10e87427710a
Vũ Lê <lehoangvu@bacoor.co>
2022-10-26 09:13:14 +0700

    add duy-demo

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
-const path = require('path');
+const http = require("http");
+const path = require("path");
 const port = 8000;
 // Use the whole root as static files to be able to serve the html file and
 // the build folder
-app.use(express.static(path.join(__dirname, '/')));
+app.use(express.static(path.join(__dirname, "/")));
 // Send html on '/'path
-app.get('/', (req, res) => {
-res.sendFile(path.join(__dirname, + '/index.html'));
-})
+app.get("/sandbox", (req, res) => {
+  res.sendFile(path.join(__dirname, +"/sandbox/index.html"));
+});
+app.get("/duy-demo", (req, res) => {
+  res.sendFile(path.join(__dirname, +"/duy-demo/index.html"));
+});
 // Create the server and listen on port
 http.createServer(app).listen(port, () => {
-console.log(`Server running on localhost:${port}`);
+  console.log(`Server running on localhost:${port}`);
 });
